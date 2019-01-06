90ce7b5fc927cc6e6e337f233683e8403e1d7239
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-01-05 23:57:18 -0200

    ADD

---
+++ language/pt_br/info_acp_upload.php
@@ -0,0 +1,23 @@
+<?php
+/**
+*
+* @package Upload Extensions
+* @copyright (c) 2014 - 2019 Igor Lavrov (https://github.com/LavIgor) and John Peskens (http://ForumHulp.com)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* Brazilian Portuguese translation by eunaumtenhoid (c) 2019 [ver 3.2.0-RC] (https://github.com/phpBBTraducoes)
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_UPLOAD_EXT_TITLE'				=> 'Upload Extensions',
+	'ACP_UPLOAD_EXT_CONFIG_TITLE'		=> 'Enviar extensões',
+));
